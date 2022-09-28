5b6d3b26a29006fcd46f965a7dfe36f502fa63bc
rachaelblannin <rachblannin:gmail.com>
2022-09-28 16:03:02 +0100

    calc

---
+++ calc.js
@@ -0,0 +1,24 @@
+function addnumbers(a, b) {
+    return a + b
+}
+
+console.log(addnumbers(4,6));
+
+function subtractnumbers(a, b) {
+    return a - b
+}
+
+console.log(subtractnumbers(4,6));
+
+
+function multiplynumbers(a, b) {
+    return a * b
+}
+
+console.log(multiplynumbers(4,6));
+
+function dividenumbers(a, b) {
+    return a / b
+}
+
+console.log(dividenumbers(20,5));
