307166ec0abea897e967c8df4546c3cd8a31bd25
rachaelblannin <rachblannin@gmail.com>
2022-09-30 12:29:24 +0100

    new commit

--- calc.js
+++ calc.js
@@ -2,25 +2,49 @@ function addnumbers(a, b) {
     return a + b
 }
 
-console.log(addnumbers(4,6));
 
 function subtractnumbers(a, b) {
     return a - b
 }
 
-console.log(subtractnumbers(4,6));
 
 
 function multiplynumbers(a, b) {
     return a * b
 }
 
-console.log(multiplynumbers(4,6));
 
 function dividenumbers(a, b) {
     return a / b
 }
 
-console.log(dividenumbers(20,5));
+let result;
+
+const calc = ("+,-,*,/");
+
+
+switch(calc) {
+    case "+":
+    result = b + b;
+    console.log("a + b = result");
+    break;
+   
+    case "-":
+    result = a + b;
+    console.log("a + b = result");
+    break;
+
+    case "*":
+    result = a + b;
+    console.log("a + b = result");
+    break;
+
+    case "/":
+    result = a + b;
+    console.log("a + b = result");
+    break;
+}
+
+
 
 
